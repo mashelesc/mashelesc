cdc3dcb140898dfec22a0d0975c35a45b6b6aaef
mashelesc <94088179+mashelesc@users.noreply.github.com>
2025-04-11 19:23:13 +0200

    deep.py

---
+++ deep.py
@@ -0,0 +1,11 @@
+"""
+An attempt to delete this file has been made, in the meantime I'll import a few packages to see what works and what does not.
+
+import X as mashele_sc
+import telegram as mashele_sc
+from edX import CS50P
+from Linux import endeavourOS as eos
+from Linux import ubuntu-24.04.3.lts
+
+Until then.
+"""
